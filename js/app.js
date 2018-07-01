d2bd7d6518460d6a9e4ca07410d7e84fe20ec9a6
Tavenner <JTavenner@apus.edu>
2018-07-01 17:24:11 -0400

    Halfway through Chapter 3

--- js/app.js
+++ js/app.js
@@ -1,8 +1,36 @@
+var Coin = /** @class */ (function () {
+    function Coin() {
+    }
+    return Coin;
+}());
+/// <reference path="./coin.ts" />
 var VendingMachine = /** @class */ (function () {
     function VendingMachine() {
+        var _this = this;
+        this.paid = ko.observable(0);
+        this.acceptCoin = function (coin) {
+            var oldTotal = _this.paid();
+            _this.paid(oldTotal + coin.value);
+        };
     }
     return VendingMachine;
 }());
 /// <reference path="vendingMachine.ts" />
 var machine = new VendingMachine();
+ko.applyBindings(machine);
+var Quarter = /** @class */ (function () {
+    function Quarter() {
+        this.value = .25;
+    }
+    // get Value() {
+    //     return this.value;
+    // }
+    // set Value(newValue: number) {
+    //     this.value = newValue;
+    // }
+    Quarter.prototype.getImageUrl = function () {
+        return "img\Quarter.png";
+    };
+    return Quarter;
+}());
 //# sourceMappingURL=app.js.map
--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -1,3 +1,9 @@
+/// <reference path="./coin.ts" />
+
 class VendingMachine {
-    
+    private paid = ko.observable(0);
+    acceptCoin = (coin: Quarter): void => {
+       let oldTotal = this.paid();
+       this.paid(oldTotal + coin.value);
+    }
 }
