323b6f4d725cdc8c3292df939d608fb535dce0ee
Tavenner <JTavenner@apus.edu>
2018-07-05 12:03:38 -0400

    Completed Chapter 3

--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -19,8 +19,11 @@ class Cell {
 
 class VendingMachine {
     paid = ko.observable(0);
+    selectedCell = ko.observable(new Cell(new CocaCola()))
     cells = ko.observableArray([])
     acceptedCoins: Coin[] = [new Dime(), new Quarter(), new Half(), new Dollar()]
+    canPay = ko.pureComputed(() => this.paid() -
+        this.selectedCell().product.price >= 0)
 
     set size(givenSize: VendingMachineSize) {
         this.cells([]);
@@ -29,8 +32,26 @@ class VendingMachine {
             this.cells.push(new Cell(product));
         }
     }
+
+    select = (cell: Cell): void => {
+        cell.sold(false)
+        this.selectedCell(cell)
+    }
     acceptCoin = (coin: Quarter): void => {
        let oldTotal = this.paid();
        this.paid(oldTotal + coin.value);
+    }
+
+    pay = (): void => {
+        if (this.selectedCell().stock() < 1) {
+            alert("I'm sorry, we're out of them!")
+            return 
+        }
+        let currentPaid = this.paid()
+        this.paid(Math.round(((currentPaid -
+            this.selectedCell().product.price)*100))/100)
+        let currentStock = this.selectedCell().stock()
+        this.selectedCell().stock(currentStock - 1)
+        this.selectedCell().sold(true)
     }
 }
